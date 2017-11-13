4f50b1452cebdb58d7b09893251b2b4a23bfbb91
prithvi <prithvisathiya@gmail.com>
2017-11-13 22:36:38 +0000

    service fixes, script fixes

--- opener.py
+++ opener.py
@@ -8,7 +8,7 @@ from passlib.hash import pbkdf2_sha256
 def verify_password(password):
 	file = 'hashedPass.txt'
 	if os.popen('whoami').read().strip() == 'pi':
-		file = '~/Documents/OpSes-Pi/hashedPass.txt'
+		file = '/home/pi/Documents/OpSes-Pi/hashedPass.txt'
 	passFile = open(file, 'r')
 	hashed = passFile.read().strip()
 	return pbkdf2_sha256.verify(password, hashed)
@@ -16,34 +16,37 @@ def verify_password(password):
 def Open_Sesame():
 	#Do motor control and shit here
 	print 'Opening'
+	sys.stdout.flush()
 	return
 
 def main():
 	HOST = ''
 	PORT = 9393
-	 
+
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	print 'Socket created'
-	 
+
 	#Bind socket to local host and port
 	try:
 	    s.bind((HOST, PORT))
 	except socket.error as msg:
 	    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 	    sys.exit()
-	     
+
 	print 'Socket bind complete'
-	 
+
 	#Start listening on socket
 	s.listen(5)
 	print 'Socket now listening'
 
+	sys.stdout.flush()
 
 	while 1:
 	    #wait to accept a connection - blocking call
 	    conn, addr = s.accept()
 	    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	    sys.stdout.flush()
 	    password = conn.recv(1024)
 	    if verify_password(password):
 	    	print 'Correct Password'
